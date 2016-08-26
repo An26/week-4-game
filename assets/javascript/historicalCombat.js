2ae4054b0efa4f0dff52adaa4c84dcc27d300238
An Huynh <An Huynh>
2016-08-26 08:14:57 -0500

    updates

---
+++ assets/javascript/historicalCombat.js
@@ -0,0 +1,24 @@
+var hero;
+var enemy;
+
+var characters= [{
+	id: 'Lincoln',
+	health: 100
+}, {
+	id: 'Einstein',
+	health: 120
+}, {
+	id: 'Susan',
+	health: 150;
+}, {
+	id: 'Gandhi',
+	health: 200
+}]
+
+function buildCharacters(){
+	for(var i = 0; i < characterList.length; i++){
+			$('.characterList')
+		}
+	} 
+
+	//builds characters... I already have them. 
