5f50c64a5527881ab9a296dda18c7d769370cbc9
An Huynh <An Huynh>
2016-08-26 23:34:54 -0500

    updates

--- assets/javascript/historicalCombat.js
+++ assets/javascript/historicalCombat.js
@@ -1,31 +1,80 @@
+
+
+
+
 var hero;
 var enemy;
 
 var characterList= [{
-	id: 'lincoln'
+	id: 'lincoln',
+	name: "Abraham Lincoln",
+	health: 100,
+	attack: 10,
+	counterAttack: 15,
+
 }, {
-	id: 'einstein'
+	id: 'einstein',
+	name: "Albert Einstein",
+	health: 110,
+	attack: 20,
+	counterAttack: 20,
 }, {
-	id: 'susan'
+	id: 'susan',
+	name: "Susan B. Anthony",
+	health: 120,
+	attack: 30,
+	counterAttack: 25,
 }, {
-	id: 'gandhi'
+	id: 'gandhi',
+	name: "Mahatma Gandhi",
+	health: 150,
+	attack: 40,
+	counterAttack: 30,
 }];
+
+function reset() {
+
+}
+
+function addClickListeners() {
+	$('.character').click(assignCharacter);
+}
+
+
+function assignCharacter() {
+	if (hero && enemy) return;
+		var html = "<p>Pick Your Character!</p>"
+		document.querySelector('.messageUser').innerHTML = html;
+		
+
+	if (hero) {
+		enemy = $(this).appendTo('#opponent');
+		var html = "<p>Your Ready to Fight!</p>"
+		document.querySelector('.messageUser').innerHTML = html;
+		$('#attackButton').show();
+		$(this).removeClass("neutral"); 
+
+
+	} else {
+		hero = $(this).appendTo('#yourCharacter');
+		var html = "<p>Pick Your Opponent!</p>"
+		document.querySelector('.messageUser').innerHTML = html;
+		$(this).removeClass("neutral"); 
+		$('.neutral').appendTo('.otherEnemies');
+	}
+}
 
 function buildCharacters() {
 	for (var i = 0; i < characterList.length; i++) {
 		var currentCharacter = characterList[i];
-		$("#characterList").append('<div class ="character">text</div>');
-	}
+		var nameDiv = $("#characterList").append('<div class ="character neutral" id = '+ currentCharacter.id +'></div>');
+		 $("<div class='imgName'></div>").html(currentCharacter.name).appendTo(nameDiv);
+		 $("<div class='imgHealth'></div>").html("HP: " + currentCharacter.health).appendTo(nameDiv);
+	} 
 }
 
-// function addClickListeners() {
-// 	$('.character').click(selectCharacter);
-// }
-
-// function selectCharacter() {
-//  console.log(this);
-// }
-
-buildCharacters();
-// addClickListeners();
-// selectCharacter();
+$(document).ready(function(){
+	buildCharacters();
+	addClickListeners();
+	assignCharacter();
+});
